c181411dd8d5d1faed348ea4b9c5d6be06f636f6
ImRevyus <77307159+ImRevyus@users.noreply.github.com>
2023-03-11 13:58:06 +0100

    fd

--- assets/main.js
+++ assets/main.js
@@ -1,31 +1,17 @@
-function render(container, message) {
+function render (container, message) {
   container.innerHTML = `<p>${message}</p>`
 }
 
-(async function() {
+(async function () {
+
   const app = document.getElementById('app')
 
-  // Render the message from the index.json file
   let index = await fetch('content/index.json')
   let { message } = await index.json()
+
   render(app, message)
 
-  // Create a section for displaying the latest posts
-  const postsSection = document.createElement('section')
-  postsSection.id = 'posts'
-  app.appendChild(postsSection)
+  const nav = document.createElement('nav')
 
-  // Render the latest posts from the posts.json file
-  let posts = await fetch('content/posts.json')
-  let { posts: latestPosts } = await posts.json()
-  latestPosts.forEach(post => {
-    const postElement = document.createElement('article')
-    postElement.innerHTML = `
-      <h2>${post.title}</h2>
-      <p>${post.date}</p>
-      <img src="${post.image}" alt="${post.title}">
-      <p>${post.description}</p>
-    `
-    postsSection.appendChild(postElement)
-  })
+  app.appendChild(nav)
 })()
--- posts/2023-03-11-hello-again.md
+++
@@ -1,6 +0,0 @@
----
-title: Hello again
-date: 2023-03-11T12:44:13.867Z
-image: media/logo-no-background.svg
-description: "Hola todos "
----
