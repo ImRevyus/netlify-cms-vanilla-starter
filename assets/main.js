45fe007010dd1a73b67f926c89d9de7059c22808
ImRevyus <77307159+ImRevyus@users.noreply.github.com>
2023-03-11 13:47:49 +0100

    d

--- assets/main.js
+++ assets/main.js
@@ -1,17 +1,31 @@
-function render (container, message) {
+function render(container, message) {
   container.innerHTML = `<p>${message}</p>`
 }
 
-(async function () {
-
+(async function() {
   const app = document.getElementById('app')
 
+  // Render the message from the index.json file
   let index = await fetch('content/index.json')
   let { message } = await index.json()
-
   render(app, message)
 
-  const nav = document.createElement('nav')
+  // Create a section for displaying the latest posts
+  const postsSection = document.createElement('section')
+  postsSection.id = 'posts'
+  app.appendChild(postsSection)
 
-  app.appendChild(nav)
+  // Render the latest posts from the posts.json file
+  let posts = await fetch('content/posts.json')
+  let { posts: latestPosts } = await posts.json()
+  latestPosts.forEach(post => {
+    const postElement = document.createElement('article')
+    postElement.innerHTML = `
+      <h2>${post.title}</h2>
+      <p>${post.date}</p>
+      <img src="${post.image}" alt="${post.title}">
+      <p>${post.description}</p>
+    `
+    postsSection.appendChild(postElement)
+  })
 })()
